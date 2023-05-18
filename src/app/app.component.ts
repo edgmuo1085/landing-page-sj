cc9c74bfa1f1f8844d9a59c886d0fe20e86d791f
Edgar Muñoz <edgmuo1085@gmail.com>
2023-05-17 20:39:52 -0500

    se realiza ajustes de formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { FilterMatchMode, PrimeNGConfig } from 'primeng/api';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'landing-example';
+  lang: string = 'es';
+  subscription!: Subscription;
+
+  constructor(private primengConfig: PrimeNGConfig, public translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    this.configPrimeNG();
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  configPrimeNG() {
+    this.translate.addLangs(['es', 'en']);
+    this.translate.setDefaultLang('es');
+
+    const browserLang = this.translate.getBrowserLang() || 'es';
+    let lang = browserLang.match(/es|en/) ? browserLang : 'es';
+    this.changeLang(lang);
+
+    this.subscription = this.translate.stream('primeng').subscribe(data => {
+      this.primengConfig.setTranslation(data);
+    });
+
+    this.primengConfig.filterMatchModeOptions = {
+      text: [
+        FilterMatchMode.STARTS_WITH,
+        FilterMatchMode.CONTAINS,
+        FilterMatchMode.NOT_CONTAINS,
+        FilterMatchMode.ENDS_WITH,
+        FilterMatchMode.EQUALS,
+        FilterMatchMode.NOT_EQUALS,
+      ],
+      numeric: [
+        FilterMatchMode.EQUALS,
+        FilterMatchMode.NOT_EQUALS,
+        FilterMatchMode.LESS_THAN,
+        FilterMatchMode.LESS_THAN_OR_EQUAL_TO,
+        FilterMatchMode.GREATER_THAN,
+        FilterMatchMode.GREATER_THAN_OR_EQUAL_TO,
+      ],
+      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER],
+    };
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
